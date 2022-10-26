2baf7fd15d83cc7675f3ad016f71f7d7e3a7f7e5
Swarnali Saha <64646994+swarnalisaha2000@users.noreply.github.com>
2022-10-26 23:15:15 +0530

    First_commit

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -27,4 +27,14 @@
 //console.log('Will read file');
 
 
-//Files
+//SERVER
+
+const http=require('http');
+const url = require('url');
+const server = http.createServer((req, res)=>{
+    console.log(req.url);
+    res.end('Hello');
+});
+server.listen(8000,'127.0.0.1',()=>{
+    console.log('Listening to request');
+});
