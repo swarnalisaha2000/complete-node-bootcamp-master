5f274199fedf22b60c35d24c81800d8f6652f8b4
Swarnali Saha <64646994+swarnalisaha2000@users.noreply.github.com>
2022-10-27 13:29:02 +0530

    building Api

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -3,7 +3,7 @@
 // const hello = 'Hello World';
 // Blocking or synchronous Code
 
-// const fs  = require('fs');
+const fs  = require('fs');
 // const textin = fs.readFileSync('./txt/input.txt','utf-8');
 // const textOut =  `This is what we know about Avocado:${textin}.\nCreated at ${Date.now()}.`
 // fs.writeFileSync('./txt/output.txt',textOut);
@@ -31,11 +31,37 @@
 
 const http=require('http');
 const url = require('url');
+
+const data=fs.readFileSync('./dev-data/data.json','utf-8');
+const productData=JSON.parse(data);
+
+
 const server = http.createServer((req, res)=>{
-    console.log(req.url);
-    res.end('Hello');
+    const pathaname = req.url;
+    if(pathaname==='/' || pathaname==='/overview'){
+        
+        res.end('Overview');
+    }
+    else if(pathaname==='/api'){
+        res.writeHead(200,{'Context-type':'application/json'});
+        //console.log(productData);
+        res.end(data);
+        //res.end('API');
+    }
+    else if(pathaname==='/product'){
+        
+        res.end('Product');
+    }
+    //console.log(req.url);
+    //res.end('Hello'); 
+    else{
+        res.writeHead(404,{
+            'Context-type':'text/html',
+            'my-header':'hello'
+        });
+        res.end('<h1>Page not found</h1>');
+    }
 });
 server.listen(8000,'127.0.0.1',()=>{
     console.log('Listening to request');
 });
-//hii
